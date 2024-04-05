025787f0d502cab3824f12a25fce69117b55d8f5
drissbejgana <93830096+drissbejgana@users.noreply.github.com>
2024-04-05 15:46:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const rendezsRouter = require("./routes/rendezsRouter")
 app.use(
 	cors({
-		origin: "https://cliniquerrahma.ma/",
+		origin: "http://127.0.0.1:5501",
 	})
 )
 app.use(express.json())
